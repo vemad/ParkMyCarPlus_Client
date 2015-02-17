8a1c7a6ec711e3f6803a1ec582197cd944e29ee2
Gaetan <gaetan.deshayes2@gmail.com>
2015-02-17 15:41:13 +0100

    First commit

---
+++ app/src/androidTest/java/com/otsims5if/pmc/pmc_android/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.otsims5if.pmc.pmc_android;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
