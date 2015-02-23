e0b994ff28e6b6a2a9fbd0388e25b9d47252d637
ilerviraragavane <iler.viraragavane@gmail.com>
2015-02-23 11:35:14 +0100

    Map working inside tab

--- app/src/main/java/com/otsims5if/pmc/pmc_android/UserMapFragment.java
+++ app/src/main/java/com/otsims5if/pmc/pmc_android/UserMapFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * Created by Iler on 18/02/2015.
@@ -24,10 +25,46 @@ public class UserMapFragment extends PlaceholderFragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
-        mapView = (MapView) rootView.findViewById(R.id.map);
+        // inflat and return the layout
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+        mapView = (MapView) v.findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
+        mapView.onResume();//needed to get the map to display immediately
+
+        /*try {
+            MapsInitializer.initialize(this);
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }*/
+
         map = mapView.getMap();
-        return rootView;
+
+        //Perform any camera updates here
+
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
