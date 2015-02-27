53a7dd32037e44fab7c6de856540ef2697364d9d
ilerviraragavane <iler.viraragavane@gmail.com>
2015-02-28 00:20:36 +0100

    Implement get available places in user range

--- app/src/main/java/api/ApiConfig.java
+++ app/src/main/java/api/ApiConfig.java
@@ -2,5 +2,6 @@ package api;
 
 
 public class ApiConfig{
-    public static final String baseUrl = "http://10.0.2.2:8080";
+    //public static final String baseUrl = "http://10.0.2.2:8080";
+    public static final String baseUrl = "http://178.62.174.107:8080";
 }
--- app/src/main/java/com/otsims5if/pmc/pmc_android/UserMapFragment.java
+++ app/src/main/java/com/otsims5if/pmc/pmc_android/UserMapFragment.java
@@ -1,32 +1,34 @@
 package com.otsims5if.pmc.pmc_android;
 
 import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.Context;
-import android.content.res.Configuration;
 import android.graphics.Color;
-import android.graphics.Point;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.Display;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+import api.place.GetListPlacesByPositionCallback;
+import api.place.Place;
+import api.place.PlaceServices;
 
 /**
  * Created by Iler on 18/02/2015.
@@ -37,7 +39,16 @@ public class UserMapFragment extends PlaceholderFragment{
     GoogleMap map;
     Button parkButton;
     Button leaveButton;
+    Switch placeFindSwitch;
     Marker currentPositionMarker;
+    LatLng myCurrentLocation;
+    Thread checkPlacesThread;
+    Handler handler = new Handler();
+    boolean startThread = false;
+    int radius = 20;
+    Circle userRange;
+
+    ArrayList<Marker> placeMarkerList = new ArrayList<Marker>();
 
     LatLng laDouaGastonBerger = new LatLng(45.781543000000010000,4.872104000000036000);
     LatLng insa = new LatLng(45.7829609,4.875031300000046);
@@ -57,23 +68,34 @@ public class UserMapFragment extends PlaceholderFragment{
         // Get buttons by there id
         parkButton = (Button) v.findViewById(R.id.parkButton);
         leaveButton = (Button) v.findViewById(R.id.leaveButton);
+        placeFindSwitch = (Switch) v.findViewById(R.id.placeFindSwitch);
 
         mapView = (MapView) v.findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
         MapsInitializer.initialize(v.getContext());
+        checkPlacesThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                while (startThread) {
+                    try {
+                        Thread.sleep(10000);
+                        handler.post(new Runnable() {
 
-        /*if(this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-            s.setThumbTextPadding(100);
-        }else{
-            s.setThumbTextPadding(50);
-        }*/
-        //mapView.onResume();//needed to get the map to display immediately
+                            @Override
+                            public void run() {
+                                // TODO Auto-generated method stub
 
-        /*try {
-            MapsInitializer.initialize(this);
-        } catch (GooglePlayServicesNotAvailableException e) {
-            e.printStackTrace();
-        }*/
+                                //Show marker place that are available inside user's range
+                                setUpMarkerListAndShowPlaces();
+                            }
+                        });
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                }
+            }
+        });
 
         //map = mapView.getMap();
         setUpMapIfNeeded(v);
@@ -100,6 +122,21 @@ public class UserMapFragment extends PlaceholderFragment{
                 currentPositionMarker.remove();
             }
         });
+        placeFindSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    startThread = true;
+                    checkPlacesThread.start();
+                    //setUpMarkerList();
+                } else {
+                    // The toggle is disabled
+                    //checkPlacesThread.interrupt();
+                    startThread = false;
+                }
+            }
+        });
+
         //Perform any camera updates here
 
         return v;
@@ -109,6 +146,12 @@ public class UserMapFragment extends PlaceholderFragment{
     public void onResume() {
         super.onResume();
         mapView.onResume();
+        try{
+            myCurrentLocation = new LatLng(map.getMyLocation().getLatitude(), map.getMyLocation().getLongitude());
+        }catch(NullPointerException e){
+
+        }
+
     }
 
     @Override
@@ -142,39 +185,73 @@ public class UserMapFragment extends PlaceholderFragment{
         }
     }
 
+    private void setUpMarkerListAndShowPlaces(){
+        //PlaceServices.getInstance().getListPlacesByPosition(45.78166386726485, 4.872752178696828, 5, new ShowListPlacesCallback()).execute();
+        Log.i("Info", "SetUpMarker");
+        try {
+            //double latitude = map.getMyLocation().getLatitude();
+            //double longitude = map.getMyLocation().getLongitude();
+            myCurrentLocation = new LatLng(map.getMyLocation().getLatitude(), map.getMyLocation().getLongitude());
+            //Draw a circle that show the range of the user
+            if(userRange!=null) {
+                userRange.remove();
+            }
+            userRange = map.addCircle(new CircleOptions()
+                    .center(myCurrentLocation)
+                    .radius(radius)
+                    .fillColor(0x40ff0000)
+                    .strokeColor(Color.RED)
+                    .strokeWidth(2));
+
+            Log.i("Info", "GetPlaces");
+
+            PlaceServices.getInstance().getListPlacesByPosition(myCurrentLocation.latitude,
+                    myCurrentLocation.longitude,
+                    radius, new ShowListPlacesCallback()).execute();
+        }catch(NullPointerException e){
+            Log.i("Error", "NullPointerExecption");
+        }
+    }
+
+    /*Method for displaying a place received by a service*/
+    private class ShowListPlacesCallback extends GetListPlacesByPositionCallback {
+        protected void callback(Exception e, Place[] places){
+            if(!placeMarkerList.isEmpty()) {
+                for (Marker marker : placeMarkerList) {
+                    marker.remove();
+                }
+                placeMarkerList.clear();
+            }
+            for(Place place : places){
+                LatLng position = new LatLng(place.getLatitude(), place.getLongitude());
+                Marker marker = map.addMarker(new MarkerOptions()
+                        .position(position)
+                        .title("Place "+place.getId())
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
+                placeMarkerList.add(marker);
+            }
+        }
+    }
+
     /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * <p/>
      * This should only be called once and when we are sure that {@link #map} is not null.
      */
     private void setUpMap() {
+        //Allow to get the local position
+        map.setMyLocationEnabled(true);
 
-        CircleOptions circleOptions = new CircleOptions();
+        //Get my position
+        //myCurrentLocation = new Location(map.getMyLocation().getLatitude(),map.getMyLocation().ge);
 
-        Circle circle = map.addCircle(new CircleOptions()
+        /*Circle circle = map.addCircle(new CircleOptions()
                 .center(doubleMixte)
                 .radius(100)
                 .fillColor(0x40ff0000)
                 .strokeColor(Color.RED)
-                .strokeWidth(2));
+                .strokeWidth(2));*/
 
-        map.addMarker(new MarkerOptions()
-                            .position(laDouaGastonBerger)
-                            .title("La Doua - Gaston Berger")
-                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
-        map.addMarker(new MarkerOptions()
-                .position(insa)
-                .title("Insa Lyon")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
-        map.addMarker(new MarkerOptions()
-                            .position(doubleMixte)
-                            .title("Double Mixte")
-                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
-        map.addMarker(new MarkerOptions()
-                            .position(laPoste)
-                            .title("La Poste")
-                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
-        map.setMyLocationEnabled(true);
+        setUpMarkerListAndShowPlaces();
     }
 }
